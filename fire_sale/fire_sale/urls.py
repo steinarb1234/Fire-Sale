2833a80c9fac3cce89da7d0599327a2f2d1a3db6
Steinar <steinarb20@ru.is>
2023-05-21 23:32:18 +0000

    nav bar to separate file

--- fire_sale/fire_sale/urls.py
+++ fire_sale/fire_sale/urls.py
@@ -20,7 +20,8 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('', include('item.url')),
     path('admin/', admin.site.urls),
-    path('listings/', include('listing.url')),
+    path('listings/', include('item.url')),
     path('categories/', include('category.url')),
 ]
