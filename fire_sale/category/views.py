dbb01f4615df9fe84d03117ba9b8ed9662a40d8c
Þórhildur Anna Ragnarsdóttir <thorhilduranna@MacBook-Pro-2.local>
2023-05-22 15:47:07 +0000

    fyrir guðbjorn

--- fire_sale/category/views.py
+++ fire_sale/category/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 categories = [
     {'name': 'Fashion', 'image': 'hat_image'},
     {'name': 'Tech', 'image': 'computer_image'},
+    {'name': 'Home', 'image': 'house_image'},
+    {'name': 'Toys', 'image': 'toy_image'},
+    {'name': 'Sports', 'image': 'sports_image'},
+    {'name': 'Books', 'image': 'book_image'},
+    {'name': 'Other', 'image': 'other_image'}
 ]
 
 # Create your views here.
