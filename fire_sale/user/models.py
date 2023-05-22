3bd23164acede06e121e026dbd20f0665fc75e8f
bjarki1312 <bjarkismarismarason@gmail.com>
2023-05-22 11:17:55 +0000

    user models

--- fire_sale/user/models.py
+++ fire_sale/user/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=255)
+    enail = models.CharField(max_length=255)
+    
+class UserInfo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=255)
+    avg_rating = models.FloatField(default=0.0)
+    image = models.CharField(max_length=9999)
+    
+class UserProfile(models.Models):
+    user_info = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
+    address = models.Charfield(max_length=255)
+    city = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=255)
+    bio = models.CharField(max_length=255)
