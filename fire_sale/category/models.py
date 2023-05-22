a23aae3af207f6144b1986a89353323b386ca830
bjarki1312 <bjarkismarismarason@gmail.com>
2023-05-22 12:29:20 +0000

    New categories

--- fire_sale/category/models.py
+++ fire_sale/category/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from user.models import User
 
+# Category
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.CharField(max_length=9999)
+
+class CategoryFavorite(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category_views = models.IntegerField(default=0)
