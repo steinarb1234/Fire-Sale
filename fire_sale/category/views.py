fa0867cdad55deafc75b160e8ee348126fd1902e
Guðbjörn <gudbjorn20@ru.is>
2023-05-22 10:49:58 +0000

    Added test categories in cate../views.py

--- fire_sale/category/views.py
+++ fire_sale/category/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
-def index(request):
-    return render(request, 'category/index.html')
+categories = [
+    {'name': 'Fashion', 'image': 'hat_image'},
+    {'name': 'Tech', 'image': 'computer_image'},
+]
 
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'category/index.html', context={
+        'categories': categories
+    })
