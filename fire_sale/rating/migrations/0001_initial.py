b7af0144ff2b733c80e915827c5b200ecad26241
Steinar <steinarb20@ru.is>
2023-05-22 14:37:20 +0000

    fixed error

--- fire_sale/rating/migrations/0001_initial.py
+++ fire_sale/rating/migrations/0001_initial.py
@@ -9,17 +9,18 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('item', '0001_initial'),
-        ('user', '0001_initial'),
+        ('offer', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='WatchListItem',
+            name='Rating',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('item', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='item.itemstats')),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='user.user')),
+                ('message', models.CharField(max_length=255)),
+                ('date', models.DateField()),
+                ('rating', models.IntegerField()),
+                ('offer', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to='offer.offer')),
             ],
         ),
     ]
