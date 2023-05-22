8af4aa6ad90cd8c1144173e3542d8cd3f3368549
bjarki1312 <bjarkismarismarason@gmail.com>
2023-05-22 17:54:56 +0000

    adding item forms

---
+++ fire_sale/item/forms/item_form.py
@@ -0,0 +1,21 @@
+from django.forms import ModelForm, widgets
+from django import forms
+from item.models import Item
+
+class CreateListingForm(forms.ModelForm):
+    condition = forms.CharField(max_length=255)
+    description = forms.CharField(max_length=9999, widget=forms.Textarea)
+    images = forms.CharField(max_length=9999, widget=forms.Textarea)
+
+    class Meta:
+        model = Item
+        fields = ['name', 'price', 'condition', 'description', 'images']
+
+class EditListingForm(forms.ModelForm):
+    condition = forms.CharField(max_length=255)
+    description = forms.CharField(max_length=9999, widget=forms.Textarea)
+    images = forms.CharField(max_length=9999, widget=forms.Textarea)
+
+    class Meta:
+        model = Item
+        fields = ['name', 'price', 'condition', 'description', 'images']
