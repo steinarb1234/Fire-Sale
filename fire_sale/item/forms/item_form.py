e9900612b0e736f44bed89ef72f1020d48e465b8
Steinar <steinarb20@ru.is>
2023-05-23 11:11:31 +0000

    item form working

--- fire_sale/item/forms/item_form.py
+++ fire_sale/item/forms/item_form.py
@@ -1,14 +1,17 @@
 from django.forms import ModelForm, widgets
 from django import forms
-from item.models import Item
+from item.models import Item, ItemDetails, ItemStats, ItemImage
 
 
 class ItemCreateForm(ModelForm):
     class Meta:
         model = Item
-        exclude = [ 'id' ]
+        exclude = ['id', 'seller']
         widgets = {
-            'name': widgets.TextInput(attrs={'class': 'form-control'})
+            'name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'price': widgets.NumberInput(attrs={'class': 'form-control'}),
+            'category': widgets.Select(attrs={'class': 'form-control'})
+
         }
 
 
