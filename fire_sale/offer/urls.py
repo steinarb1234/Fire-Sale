f3b78b4a7d5c2e93ac8c3c2f8a820ff351f1cd95
Steinar <steinarb20@ru.is>
2023-05-23 13:32:11 +0000

    d

--- fire_sale/offer/urls.py
+++ fire_sale/offer/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # http://localhost:8000/offers
     path('', views.index, name="offer-index"),
     # path('<int:id>', views.get_item_by_id, name="item-details")
-    # path('create_item', views.create_item, name="create_item")
+    path('checkout', views.checkout, name="checkout"),
 ]
 
 
