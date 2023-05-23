898950aaa8c7f9cf44b2c7f7ed467d85dacea979
Steinar <steinarb20@ru.is>
2023-05-23 11:57:56 +0000

    fixed merge

--- fire_sale/item/forms/item_form.py
+++ fire_sale/item/forms/item_form.py
@@ -2,19 +2,10 @@ from django.forms import ModelForm, widgets
 from django import forms
 from item.models import Item, ItemDetails, ItemStats, ItemImage
 
-
-<<<<<<< HEAD
 class CreateItemForm(ModelForm):
     image = forms.URLField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
     condition = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
     description = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-=======
-class ItemCreateForm(ModelForm):
-    
-    condition = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    description = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    images = forms.ImageField(required=True, widget=forms.ClearableFileInput(attrs={'class': 'form-control'}))
->>>>>>> 0e5f1d32211c5c1a75e25bb45225c2b94a76f4c4
     class Meta:
         model = Item
         exclude = ['id']
@@ -24,18 +15,6 @@ class ItemCreateForm(ModelForm):
             'category': widgets.Select(attrs={'class': 'form-control'})
         }
 
-<<<<<<< HEAD
-=======
-class CreateItemForm(forms.ModelForm):
-    condition = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    description = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    images = forms.ImageField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    
-    class Meta:
-        model = Item
-        exclude = ['id', 'category', 'seller']
-        fields = ['name', 'price']
->>>>>>> 0e5f1d32211c5c1a75e25bb45225c2b94a76f4c4
 
 class EditItemForm(forms.ModelForm):
     condition = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
