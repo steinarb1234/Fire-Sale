4059c3dd32fab85362f3da795d467892226f4c31
Guðbjörn <gudbjorn20@ru.is>
2023-05-24 10:14:41 +0000

    Added sort functionality to 'Categories' pages

---
+++ fire_sale/static/js/sort.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', () => {
+  
+  // Store references to the items container and sorting select element
+  const itemsContainer = document.getElementsByClassName('card-grid')[0];
+  const sortingSelect = document.getElementById('sort-selector');
+
+  // Don't run the code if there is nothing to sort
+  if (itemsContainer === null || sortingSelect === null) {
+    return;
+  }
+
+  // Function to sort the items based on the selected option
+  const sortItems = () => {
+    const selectedOption = sortingSelect.value;
+    let items = Array.from(itemsContainer.getElementsByClassName('card'));
+
+    // Sorting based on the selected option
+    items.sort((a, b) => {
+      let aValue, bValue;
+
+      if (selectedOption === 'name-a-z' || selectedOption === 'name-z-a') {
+        // Sort by name
+        aValue = a.querySelector('.card-title').textContent.toLowerCase();
+        bValue = b.querySelector('.card-title').textContent.toLowerCase();
+      } else if (selectedOption === 'price-hi-lo' || selectedOption === 'price-lo-hi') {
+        // Sort by price
+        aValue = parseFloat(a.querySelector('.card-text').textContent.replace('$', ''));
+        bValue = parseFloat(b.querySelector('.card-text').textContent.replace('$', ''));
+      }
+
+      if (selectedOption === 'name-z-a' || selectedOption === 'price-hi-lo') {
+        // Sort in descending order
+        return (aValue < bValue) ? 1 : -1;
+      } else {
+        // Sort in ascending order
+        return (aValue > bValue) ? 1 : -1;
+      }
+    });
+
+    // Append sorted items to the container
+    items.forEach(item => {
+      itemsContainer.appendChild(item);
+    });
+  };
+
+  // Sort items whenever the select option changes
+  sortingSelect.addEventListener('change', sortItems);
+
+  // Initial sorting
+  sortItems();
+});
