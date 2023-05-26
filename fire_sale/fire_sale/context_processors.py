963c8827e138f9c93ebe13238e5476bd42dfc63e
Guðbjörn <gudbjorn20@ru.is>
2023-05-26 18:39:44 +0000

    Added profile_image to context_processor

--- fire_sale/fire_sale/context_processors.py
+++ fire_sale/fire_sale/context_processors.py
@@ -1,6 +1,17 @@
 
 from category.models import Category
+from user.models import User
 
 def categories_processor(request):
     categories = Category.objects.all()
     return {'categories': categories}
+
+def navigation_bar_processor(request):
+    user = request.user
+    profile_image_url = ''
+    
+    if user.is_authenticated and hasattr(user, 'username'):
+        user_instance = User.objects.get(user_name = user.username)
+        profile_image_url = user_instance.userinfo.image
+    
+    return {'profile_image_url': profile_image_url}
