2d8df26d98a8cd982caad4a45cb93caf2cd434fe
bjarki1312 <bjarkismarismarason@gmail.com>
2023-05-27 01:51:44 +0000

    userProfile registration finished

--- fire_sale/user/forms/user_form.py
+++ fire_sale/user/forms/user_form.py
@@ -10,19 +10,21 @@ class CustomUserCreationForm(ModelForm):
     class Meta:
         model = User
         fields = ['full_name', 'email']
-
+        widgets = {
+            'full_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+        }
 
 class UserProfileForm(ModelForm):
     class Meta:
         model = UserProfile
-        exclude = ['id']
+        exclude = ['id', 'user_info']
         # Bæta við database fyrir countries
         widgets = {
-            'country': widgets.Select(attrs={'class': 'form-control'}),
+            'country': widgets.TextInput(attrs={'class': 'form-control'}),
             'address': widgets.TextInput(attrs={'class': 'form-control'}),
             'city': widgets.TextInput(attrs={'class': 'form-control'}),
             'zip-code': widgets.TextInput(attrs={'class': 'form-control'}),
             'bio': widgets.TextInput(attrs={'class': 'form-control'}),
-            # Athuga með userInfo á þessari síður...
             'user_info': widgets.TextInput(attrs={'class': 'form-control'}),
         }
--- fire_sale/user/views.py
+++ fire_sale/user/views.py
@@ -4,33 +4,43 @@ from django import forms
 from user.forms.user_form import CustomUserCreationForm, UserProfileForm
 from user.models import UserProfile, User, UserInfo
 
-
+# Bæta við öllum upplýsingum...
 def register(request):
     if request.method == 'POST':
         
         user_creation_form = UserCreationForm(request.POST)
         user_info_creation_form = CustomUserCreationForm(request.POST)
-        
-        if user_creation_form.is_valid() and user_info_creation_form.is_valid():
+        user_profile_form = UserProfileForm(request.POST)
+        if user_creation_form.is_valid() and user_info_creation_form.is_valid() and user_profile_form.is_valid():
             
-            # Fill in first form
+            # Save user form
             user_form = user_creation_form.save()
             
-            # Fill in second form
+            # Save custom user info form
             user_info_creation_form.instance.user_name = user_form.username
-            user_info_creation_form.save()
-
-            # Want to fill the UserInfo() module with nothing, but make the id from the first form saved
+            custom_user_form = user_info_creation_form.save()
+            
+            # Create and save UserInfo
             user_info = UserInfo()
             user_info.user_id = User.objects.get(user_name = user_form.username).id
             user_info.save()
+            
+            # Save user profile form
+            # Attach user_info instance to UserProfile instance before saving
+            user_profile_form.instance.user_info = user_info
+            user_profile_form.save()
             
             return redirect('login')
     else:
         user_creation_form = UserCreationForm()
         user_info_creation_form = CustomUserCreationForm()
+        user_profile_form = UserProfileForm()
     
-    return render(request, 'user/register.html', {'user_creation_form': user_creation_form, 'user_info_creation_form': user_info_creation_form})
+    return render(request, 'user/register.html', 
+                  {'user_creation_form': user_creation_form, 
+                   'user_info_creation_form': user_info_creation_form, 
+                   'user_profile_form': user_profile_form})
+
 
 def profile(request):
     return render(request, 'user/profile.html', {
