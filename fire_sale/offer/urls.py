a253f5881ebea04c6fef069e65a982784960cbf7
Þórhildur Anna Ragnarsdóttir <thorhilduranna@MacBook-Pro-2.local>
2023-05-29 12:40:21 +0000

    checkout

--- fire_sale/offer/urls.py
+++ fire_sale/offer/urls.py
@@ -5,8 +5,10 @@ urlpatterns = [
     # http://localhost:8000/offers
     # path('', views., name="offer-index"),
     path('<int:offer_id>', views.offer_details, name="offer-details"),
-    path('checkout', views.checkout, name="checkout"),
+    path('<int:offer_id>/checkout', views.checkout, name="checkout"),
     path('create_offer/<int:item_id>', views.create_offer, name="create_offer"),
+    path('<int:offer_id>/checkout/payment', views.payment, name="payment"),
+    path('<int:offer_id>/checkout/payment/review', views.review, name="review"),
 ]
 
 
