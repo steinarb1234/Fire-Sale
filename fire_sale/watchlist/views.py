7388b4d4864c811157aa2725bfa5f4b4c227c53b
Guðbjörn <gudbjorn20@ru.is>
2023-05-30 11:49:39 +0000

    Red hearts, Remove from watchlist & appropr button

--- fire_sale/watchlist/views.py
+++ fire_sale/watchlist/views.py
@@ -1,31 +1,31 @@
-#from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import WatchListItem
 from .forms.watchlist_form import WatchListCreationForm
 from item.models import Item
 
-#@login_required
-# Fillir whatclist út frá database
-
-from django.shortcuts import redirect, reverse
-
-from django.shortcuts import redirect, reverse
-
-
-def add_to_watchlist(request, itemid):
+@login_required
+def add_to_watchlist(request, item_id):
 
     if request.method == 'POST':
         print('posting')
         watchlist_form = WatchListCreationForm()
         watchlist = watchlist_form.save(commit=False)
-        watchlist.item_id = itemid
+        watchlist.item_id = item_id
         watchlist.user_id = request.user.id
         watchlist.save()
-        return redirect('item-details', id=itemid)
+        return redirect('item-details', id=item_id)
 
-    return redirect('item-details', id=itemid)
+    return redirect('item-details', id=item_id)
 
 
+@login_required
+def delete_from_watchlist(request, item_id):
+    watchlist_item = get_object_or_404(WatchListItem, item_id=item_id, user_id=request.user.id)
+    watchlist_item.delete()
+    return redirect('item-details', id=item_id)
+
+@login_required
 def index(request):
     watchlist = WatchListItem.objects.filter(user=request.user.id)
     item_ids = watchlist.values_list('item_id', flat=True)
